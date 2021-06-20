d088881abf62e85edfc923f9d2adbb29c9d52cf0
Nikita Povalishev <nikita.povalishev@skltech.ru>
2021-06-20 16:58:28 +0300

    Add some static files for sales pages

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -1,1 +1,9 @@
 console.log('Script from Django.');
+
+const reportButton = document.getElementById('report-btn');
+const graphImage = document.getElementById('graph-img');
+
+
+if (graphImage) {
+  reportButton.classList.remove('d-none');
+}
