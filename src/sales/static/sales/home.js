02cbd4a0043229b2281a40fca15b0ca4afc0bf9e
Nikita Povalishev <nikita.povalishev@skltech.ru>
2021-06-27 21:33:39 +0300

    Client: improve modal form with graph

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -2,8 +2,13 @@ console.log('Script from Django.');
 
 const reportButton = document.getElementById('report-btn');
 const graphImage = document.getElementById('graph-img');
-
+const reportModalBody = document.getElementById('report-modal');
 
 if (graphImage) {
   reportButton.classList.remove('d-none');
 }
+
+
+reportButton.addEventListener('click', function() {
+  reportModalBody.prepend(graphImage);
+});
