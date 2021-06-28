26ead84fc84b617266ed2382aec29593bf69a0e6
Nikita Povalishev <nikita.povalishev@skltech.ru>
2021-06-28 13:45:22 +0300

    Create PDF file for reports

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -3,6 +3,7 @@ from .views import (
   create_report_view,
   ReportListView,
   ReportDetailView,
+  render_pdf_view,
 )
 
 app_name = 'reports'
@@ -10,5 +11,6 @@ app_name = 'reports'
 urlpatterns = [
   path('', ReportListView.as_view(), name='main'),
   path('save/', create_report_view, name='create-reports'),
-  path('<pk>/', ReportDetailView.as_view(), name='detail')
+  path('<pk>/', ReportDetailView.as_view(), name='detail'),
+  path('<pk>/pdf/', render_pdf_view, name='pdf'),
 ]
--- src/reports/views.py
+++ src/reports/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 from profiles.models import Profile
 
@@ -38,3 +43,30 @@ def create_report_view(request):
   return JsonResponse({'msg': 'something went wrong.'})
 
 
+def render_pdf_view(request, pk):
+    template_path = 'reports/pdf.html'
+    obj = get_object_or_404(Report, pk=pk)
+    context = {
+      'pdfTitle': 'PDF Document',
+      'object': obj,
+    }
+
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+
+    # id downloading file
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # if displaying file
+    response['Content-Disposition'] = 'filename="report.pdf"'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
